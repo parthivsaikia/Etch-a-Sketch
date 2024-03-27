0b1161861924cfce744b6f35ad59184ef0e263de
Parthiv <parthivsaikia985@gmail.com>
2024-03-27 23:59:23 +0530

    created etch-a-sketch

---
+++ index.js
@@ -0,0 +1,46 @@
+const grid_container = document.querySelector('.grid-container');
+let grid_item;
+const colors = [
+  'AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue',
+  'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson',
+  'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange',
+  'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue',
+  'DimGray', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod',
+  'Gray', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender',
+  'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGreen', 'LightPink',
+  'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta',
+  'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed',
+  'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange',
+  'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink'
+];
+// let colorOn = false ;
+
+
+function drawGrid(cols, rows) {
+  for (let i = 1; i <= cols * rows; i++) {
+    grid_item = document.createElement("div");
+    grid_container.appendChild(grid_item);
+    grid_item.classList.add('grid-item');
+    grid_item.style.height = 900 / rows + "px";
+    grid_item.style.width = 900 / cols + "px";
+    colorOn = false;
+    // Add event listener to each grid-item
+    grid_item.addEventListener('mouseover', changeColor);
+
+
+  }
+}
+
+function changeColor(event) {
+  const clickedItem = event.target;
+  clickedItem.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+  // colorOn = true;
+}
+
+
+
+let cols = prompt("Enter the no. of columns: ");
+let rows = prompt("Enter the no. of rows: ");
+drawGrid(cols, rows);
+
+// const clearbtn = document.querySelector(".clear");
